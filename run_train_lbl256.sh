0a98bcfcf94c8381452a39c923b7c2bf73d541cb
orkunkinay <96480675+orkunkinay@users.noreply.github.com>
2026-06-18 17:34:10 +0300

    Update run_train_lbl256.sh

--- run_train_lbl256.sh
+++ run_train_lbl256.sh
@@ -3,9 +3,9 @@
 #SBATCH --partition=Teaching
 #SBATCH --output=logs/llada_train_%j.out
 #SBATCH --error=logs/llada_train_%j.err
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --mem=32G
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:h200_3g.71gb:1
 
 cd ~/msc_project/ml-rl-dllm
 source .venv/bin/activate
@@ -36,7 +36,7 @@ else:
 PY
 
 python -m train.train \
-    --config configs/experiment_configs/llada_8b_instruct_dit_confidence_BL256_mixture_safe.yaml \
+    --config configs/experiment_configs/llada_8b_instruct_dit_confidence_BL256_mixture.yaml \
     --overwrite \
     --log_memory \
     --memory_log_interval 50 \
