8ac4816904db9721070c9f34a5cbc1d7d3f809bb
orkunkinay <96480675+orkunkinay@users.noreply.github.com>
2026-07-28 20:05:18 +0300

    Change GPU resource allocation in run_eval script

--- scripts/run_eval.sh
+++ scripts/run_eval.sh
@@ -5,7 +5,7 @@
 #SBATCH --error=logs/llada_eval_%j.err
 #SBATCH --time=48:00:00
 #SBATCH --mem=32G
-#SBATCH --gres=gpu:h200_3g.71gb:1
+#SBATCH --gres=gpu:h200_1g.18gb:1
 #
 # For licensing see accompanying LICENSE file.
 # Copyright (C) 2026 Apple Inc. All Rights Reserved.
